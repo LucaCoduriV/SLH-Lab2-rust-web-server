10347cde75b7593473ecf906d2b4e28fdd187f4d
Luca Coduri <coduri.vitor@gmail.com>
2022-12-17 12:30:41 +0100

    password update

--- src/web_auth.rs
+++ src/web_auth.rs
@@ -1,5 +1,4 @@
-use std::borrow::BorrowMut;
-use crate::db::{DbConn, get_user, save_user, user_exists};
+use crate::db::{DbConn, get_user, save_user, update_password, user_exists};
 use crate::models::{
     AppState, LoginRequest, OAuthRedirect, PasswordUpdateRequest, RegisterRequest,
 };
@@ -14,15 +13,11 @@ use axum_extra::extract::CookieJar;
 use axum_sessions::async_session::{MemoryStore, Session, SessionStore};
 use serde_json::json;
 use std::error::Error;
-use std::str::FromStr;
-use axum::body::BoxBody;
 use jsonwebtoken::{encode, EncodingKey, Header};
 
-use oauth2::{AuthorizationCode, AuthUrl, ClientId, ClientSecret, CsrfToken, PkceCodeChallenge, PkceCodeVerifier, RedirectUrl, Scope, TokenResponse, TokenUrl};
-use oauth2::basic::BasicClient;
-use oauth2::reqwest::{async_http_client, http_client};
+use oauth2::{AuthorizationCode, CsrfToken, PkceCodeChallenge, PkceCodeVerifier, Scope};
+use oauth2::reqwest::{async_http_client};
 use crate::oauth::get_google_oauth_email;
-use crate::schema::users::password;
 
 /// Declares the different endpoints
 /// state is used to pass common structs to the endpoints
@@ -45,8 +40,6 @@ async fn login(
     jar: CookieJar,
     Json(login): Json<LoginRequest>,
 ) -> Result<(CookieJar, AuthResult), Response> {
-    // TODO: Implement the login function. You can use the functions inside db.rs to check if
-    //       the user exists and get the user info.
     let _email = login.login_email;
     let _password = login.login_password;
 
@@ -74,7 +67,6 @@ async fn register(
     Json(register): Json<RegisterRequest>,
 ) -> Result<AuthResult, Response> {
     // TODO: Implement the register function. The email must be verified by sending a link.
-    //       You can use the functions inside db.rs to add a new user to the DB.
     let _email = register.register_email;
     let _password = register.register_password;
 
@@ -83,12 +75,11 @@ async fn register(
     let hash = argon2::hash_encoded(_password.as_bytes(), salt, &config).unwrap();
 
     if let Err(_) = user_exists(&mut _conn, _email.as_str()) {
-        save_user(&mut _conn, User::new(_email.as_str(), hash.as_str(), AuthenticationMethod::Password, true));
+        save_user(&mut _conn, User::new(_email.as_str(), hash.as_str(), AuthenticationMethod::Password, true))
+            .or(Err((StatusCode::INTERNAL_SERVER_ERROR, AuthResult::Error).into_response()))?;
         println!("User created !");
         Ok(AuthResult::Success)
     } else {
-        // create a wrong insert to db
-        println!("User already exists");
         Err((StatusCode::UNAUTHORIZED, AuthResult::Error).into_response())
     }
 
@@ -106,20 +97,6 @@ async fn google_oauth(
     jar: CookieJar,
     State(_session_store): State<MemoryStore>,
 ) -> Result<(CookieJar, Redirect), StatusCode> {
-    // TODO: This function is used to authenticate a user with Google's OAuth2 service.
-    //       We want to use a PKCE authentication flow, you will have to generate a
-    //       random challenge and a CSRF token. In order to get the email address of
-    //       the user, use the following scope: https://www.googleapis.com/auth/userinfo.email
-    //       Use Redirect::to(url) to redirect the user to Google's authentication form.
-
-    // let client = crate::oauth::OAUTH_CLIENT.todo();
-
-    // If you need to store data between requests, you may use the session_store. You need to first
-    // create a new Session and store the variables. Then, you add the session to the session_store
-    // to get a session_id. You then store the session_id in a cookie.
-
-    // Create an OAuth2 client by specifying the client ID, client secret, authorization URL and
-    // token URL.
     let client = &crate::oauth::OAUTH_CLIENT;
 
     // Generate a PKCE challenge.
@@ -152,12 +129,6 @@ async fn google_oauth(
         .http_only(true)
         .finish();
     let jar = jar.add(cookie);
-    // This is the URL you should redirect the user to, in order to trigger the authorization
-    // process.
-
-    // Once the user has been redirected to the redirect URL, you'll have access to the
-    // authorization code. For security reasons, your code should verify that the `state`
-    // parameter returned by the server matches `csrf_state`.
 
 
     Ok((jar, Redirect::to(auth_url.as_str())))
@@ -219,11 +190,28 @@ async fn oauth_redirect(
 /// POST /password_update
 /// BODY { "old_password": "pass", "new_password": "pass" }
 async fn password_update(
-    _conn: DbConn,
+    mut _conn:DbConn,
     _user: UserDTO,
     Json(_update): Json<PasswordUpdateRequest>,
 ) -> Result<AuthResult, Response> {
-    // TODO: Implement the password update function.
+    let user = get_user(&mut _conn, _user.email.as_str())
+        .or(Err((StatusCode::BAD_REQUEST, AuthResult::Error).into_response()))?;
+
+    if user.get_auth_method() == AuthenticationMethod::OAuth {
+        return Err((StatusCode::BAD_REQUEST, AuthResult::Error).into_response());
+    }
+
+    if  !argon2::verify_encoded(user.password.as_str(), _update.old_password.as_bytes()).unwrap() {
+        return Err((StatusCode::BAD_REQUEST, AuthResult::Error).into_response());
+    }
+
+    let salt = b"randomsalt";
+    let config = argon2::Config::default();
+    let hash = argon2::hash_encoded(_update.new_password.as_bytes(), salt, &config).unwrap();
+
+    update_password(&mut _conn, _user.email.as_str(), hash.as_str())
+        .or(Err((StatusCode::INTERNAL_SERVER_ERROR, AuthResult::Success).into_response()))?;
+
     Ok(AuthResult::Success)
 }
 
@@ -239,7 +227,10 @@ fn add_auth_cookie(jar: CookieJar, _user: &UserDTO) -> Result<CookieJar, Box<dyn
     // TODO: You have to create a new signed JWT and store it in the auth cookie.
     //       Careful with the cookie options.
     let token = encode(&Header::default(), _user, &EncodingKey::from_secret("secret".as_ref()))?;
-    Ok(jar.add(Cookie::build("auth", token).finish()))
+    Ok(jar.add(Cookie::build("auth", token).path("/")
+        .secure(true)
+        .http_only(true)
+        .finish()))
 }
 
 enum AuthResult {
